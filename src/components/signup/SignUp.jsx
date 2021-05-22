107462b652eeb428e8e41398ff3478b7f3df1377
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-05-22 16:08:19 -0700

    Update SignUp.jsx

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -4,11 +4,11 @@ import { Dialog } from '@material-ui/core';
 import Form from './SignUpForm';
 
 const SignUp = ({ open, handleClose }) => (
-  <>
+  <div>
     <Dialog open={open} onClose={handleClose}>
       <Form handleClose={handleClose} open={open} />
     </Dialog>
-  </>
+  </div>
 );
 
 export default SignUp;
