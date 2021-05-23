209cae26109eaaa36111b3b4c7340d179f25e0f5
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-05-22 17:52:23 -0700

    Added login functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,28 @@
 import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
-import SignUp from './signup/SignUp';
-import Login from './login/Login';
+import LoginSignup from './login-signup/LoginSignup';
 import './App.css';
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const [loginOpen, setLoginOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // function to handle modal open
-  const handleOpen = () => {
-    setOpen(true);
+  const populate = () => {
+    if (!isLoggedIn) {
+      return (
+        <div>
+          <h1>Hello, Learners!</h1>
+          <LoginSignup
+            setUsername={setUsername}
+            setIsLoggedIn={setIsLoggedIn}
+            isLoggedIn={isLoggedIn}
+          />
+        </div>
+      );
+    }
+    return <h1>helloworld</h1>;
   };
 
-  // function to handle modal close
-  const handleClose = () => {
-    setOpen(false);
-    setLoginOpen(false);
-  };
-
-  const handleLogin = () => {
-    setLoginOpen(true);
-  };
-
-  return (
-    <div className="App">
-      <h1>Hello, Learners!</h1>
-      <Button variant="contained" color="primary" onClick={handleOpen}>
-        Sign Up
-      </Button>
-      <Button variant="contained" color="primary" onClick={handleLogin}>
-        Login
-      </Button>
-      <SignUp open={open} handleClose={handleClose} />
-      <Login open={loginOpen} handleClose={handleClose} />
-    </div>
-  );
+  return <div className="App">{populate()}</div>;
 };
 
 export default App;
