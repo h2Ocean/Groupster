4a031d72785b90488ee798ba9828cd0f7eaf2ea0
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-05-25 12:44:20 -0700

    Got sending messages working

--- src/components/GroupChat/MessageWindow/MessageWindow.jsx
+++ src/components/GroupChat/MessageWindow/MessageWindow.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import Chat from './Chat/Chat';
 
 const MessageWindow = (props) => {
+  const [{ nick }] = useState(props);
   const [{ username }] = useState(props);
   const [{ client }] = useState(props);
 
   return (
     <div id="MessageWindow" style={{ border: '1px solid purple' }}>
-      <Chat client={client} username={username} />
+      <Chat nick={nick} client={client} username={username} />
     </div>
   );
 };
