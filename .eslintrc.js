3795cbc76a25c0a2a3c2931489bfd2a48d1791d5
Nathaniel Fung <nath0127@gmail.com>
2021-05-25 19:58:17 -0700

    merge main

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'linebreak-style': 0,
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    'object-curly-newline': 'off',
-  },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
